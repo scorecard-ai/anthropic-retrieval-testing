6aad94ae8317ac5fbb4dc3c50485e8c58a378632
Jesse Hu <huyouare@users.noreply.github.com>
2023-09-21 13:56:28 -0700

    Update testset ID for testing

--- demo/wikipedia_retrieval.py
+++ demo/wikipedia_retrieval.py
@@ -7,7 +7,9 @@ from claude_retriever.searcher.searchtools.wikipedia import WikipediaSearchTool
 ANTHROPIC_SEARCH_MODEL = "claude-2"
 
 
-def generate_completion(query: str):
+def generate_completion(
+    query: str, n_search_results_to_use: int = 1, max_searches_to_try: int = 3
+):
     wikipedia_search_tool = WikipediaSearchTool()
 
     client = claude_retriever.ClientWithRetrieval(
@@ -16,14 +18,26 @@ def generate_completion(query: str):
         search_tool=wikipedia_search_tool,
     )
 
-    query = "Can you explain what LK-99 is?"
-
     response, context, prompt = client.completion_with_retrieval(
         query=query,
         model=ANTHROPIC_SEARCH_MODEL,
-        n_search_results_to_use=1,
-        max_searches_to_try=3,
+        n_search_results_to_use=n_search_results_to_use,
+        max_searches_to_try=max_searches_to_try,
         max_tokens_to_sample=1000,
     )
 
     return response, str(context)[:1000], prompt
+
+
+if __name__ == "__main__":
+    queries = [
+        "Who won the 2024 presidential election in the US?",
+        "Who won the 2023 NBA Finals?",
+        "Which team lost the 2023 NBA Finals?",
+        "How many games were there in the 2023 NBA Finals?",
+        "How much total funding has Anthropic raised?",
+    ]
+    for query in queries:
+        print("QUERY:", query)
+        response, context, prompt = generate_completion(query, 3, 5)
+        print("RESPONSE:", response, "\n\n\n")
--- tests/test_wiki_e2e.py
+++ tests/test_wiki_e2e.py
@@ -27,7 +27,6 @@ def run_all_tests(input_testset_id: int, scoring_config_id: int, model_params: d
         response, context, prompt = wikipedia_retrieval.generate_completion(
             testcase["user_query"]
         )
-        print("PROMPT:", prompt)
         # Log the result back to Scorecard for scoring
         scorecard.log_record(
             run_id=run_id,
@@ -43,8 +42,8 @@ def run_all_tests(input_testset_id: int, scoring_config_id: int, model_params: d
 
 
 if __name__ == "__main__":
-    INPUT_TESTSET_ID = 125
-    SCORING_CONFIG_ID = 2
+    INPUT_TESTSET_ID = 127
+    SCORING_CONFIG_ID = 9
     MODEL_PARAMS = {
         "model_name": "claude-2",
         "n_search_results_to_use": 1,
